bb8479f83da737474650a344da7edcbff0f0c7f1
dawodahmadi <118851483+dawodahmadi@users.noreply.github.com>
2023-11-20 16:00:28 -0500

    Starting App

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { View , Text, StyleSheet, Image } from 'react-native'; // Added Image import
+import { Provider } from 'react-redux';
+import { store } from './store';
+import HomeScreen from './src/screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import MapScreen from './src/screens/MapScreen';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  console.log(store, 'store');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider> 
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false}} />
+            <Stack.Screen name="MapScreen" component={MapScreen} options={{ headerShown: false}} />
+
+          </Stack.Navigator>
+
+        
+      </SafeAreaProvider>
+      </NavigationContainer>
+      
+      
+    </Provider>
   );
 }
 
@@ -15,7 +38,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'center', 
     justifyContent: 'center',
-  },
+  }
 });
+
+
+
+
+
+
