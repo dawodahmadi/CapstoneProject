765907d82d3bee2ec95b310e3d86f87329fc410c
dawodahmadi <118851483+dawodahmadi@users.noreply.github.com>
2023-12-21 15:22:48 -0500

    added test files

---
+++ ios/MyAppUITests.swift
@@ -0,0 +1,18 @@
+//
+//  MyAppUITests.swift
+//  capstoneproject
+//
+//  Created by dawod ahmadi on 2023-12-21.
+//
+
+import XCTest
+@testable import capstoneproject // Replace 'YourAppName' with the name of your app's module
+
+class MyAppTests: XCTestCase {
+
+    func testExample() {
+        // This is a basic test that always passes
+        XCTAssertEqual(1, 1, "One should equal one.")
+    }
+
+}
---
+++ ios/capstoneprojectTests/capstoneprojectTests.swift
@@ -0,0 +1,19 @@
+//
+//  capstoneprojectTests.swift
+//  capstoneprojectTests
+//
+//  Created by dawod ahmadi on 2023-12-21.
+//
+
+
+import XCTest
+@testable import capstoneproject
+
+final class capstoneprojectTests: XCTestCase {
+
+  func testExample() {
+      // This is a basic test that always passes
+      XCTAssertEqual(1, 1, "One should equal one.")
+  }
+
+}
